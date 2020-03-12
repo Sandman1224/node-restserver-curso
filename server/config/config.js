e938819906460161031f0a1fde8beeb765c80b81
Martin Ganami <martinganami@gmail.com>
2020-03-11 22:41:02 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -13,10 +13,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ====================
 let urlDB;
 
-//if(process.env.NODE_ENV === 'dev'){
-//    urlDB = 'mongodb://localhost:27017/cafe';
-//}else{
-    urlDB = 'mongodb+srv://strider:0Llx4rrMJMVFOYG9@cluster0-b3vsr.mongodb.net/cafe';
-//}
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
